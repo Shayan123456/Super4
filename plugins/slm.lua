6ecd728fe7e447d5d2c833234c4fc19e988e33c7
Shayan123456 <shayan_jon123@yahoo.com>
2016-03-02 22:16:49 +0330

    Update slm.lua

--- plugins/slm.lua
+++ plugins/slm.lua
@@ -1,32 +1,70 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---                                              --
---------------------------------------------------
-
 do
 
-function run(msg, matches)
-	if msg.text == "hi" then
-  return 'Shayan123Hacker'
+-- Recursive function
+local function getRandomButts(attempt)
+  attempt = attempt or 0
+  attempt = attempt + 1
+
+  local res,status = http.request("http://api.obutts.ru/noise/1")
+
+  if status ~= 200 then return nil end
+  local data = json:decode(res)[1]
+
+  -- The OpenBoobs API sometimes returns an empty array
+  if not data and attempt <= 3 then
+    print('Cannot get that butts, trying another one...')
+    return getRandomButts(attempt)
+  end
+
+  return 'http://media.obutts.ru/' .. data.preview
 end
 
-function run(msg, matches)
-	if msg.text == "salam" then
-  return 'Saaalaaaammm'
+local function getRandomBoobs(attempt)
+  attempt = attempt or 0
+  attempt = attempt + 1
+
+  local res,status = http.request("http://api.oboobs.ru/noise/1")
+
+  if status ~= 200 then return nil end
+  local data = json:decode(res)[1]
+
+  -- The OpenBoobs API sometimes returns an empty array
+  if not data and attempt < 10 then 
+    print('Cannot get that boobs, trying another one...')
+    return getRandomBoobs(attempt)
+  end
+
+  return 'http://media.oboobs.ru/' .. data.preview
+end
+
+local function run(msg, matches)
+  local url = nil
+  
+  if matches[1] == "salam" then
+    return 'saaalaaaam'
+  end
+
+  if matches[1] == "shayan" then
+    return 'Shayan123Hacker'
+  end
+
+  if url ~= nil then
+    local receiver = get_receiver(msg)
+    send_photo_from_url(receiver, url)
+  else
+    return 'Error' 
+  end
 end
 
 return {
+  description = "Gets a random boobs or butts pic", 
+  usage = {
+    "!boobs: Get a boobs NSFW image. ًں”‍",
+    "!butts: Get a butts NSFW image. ًں”‍"
+  },
   patterns = {
-    "^salam$"
-    "^hi$"
+    "^salam$",
+    "^shayan$"
   }, 
   run = run 
 }
